8541e6d4bfc06f3dee992b16c53a21f773ef293e
Arthur Orlando Vaso <atrvaso@hotmail.com>
2022-01-26 20:31:12 -0300

    Adicionado estrutura condicional e javadoc

--- Java-Projects/java-ecosystem/src/com/dio/MyFirstProgram.java
+++ Java-Projects/java-ecosystem/src/com/dio/MyFirstProgram.java
@@ -1,12 +1,15 @@
 package com.dio;
 
+import java.math.BigDecimal;
+
 import com.dio.base.Order;
 
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
 		
-		final Order order = new Order("code1234");
+		final Order order = new Order("code1234",  new BigDecimal("199.90"));
+		
 		System.out.println(order);
 
 	}
--- Java-Projects/java-ecosystem/src/com/dio/base/Order.java
+++ Java-Projects/java-ecosystem/src/com/dio/base/Order.java
@@ -1,14 +1,51 @@
 package com.dio.base;
 
+import java.math.BigDecimal;
+
+/**
+ * 
+ * @author arthur
+ * @version 1.0.0
+ * @see BigDecimal
+ * @since release 1.0.0
+ */
+
 public class Order {
 	private final String code;
+	private final BigDecimal totalValue;
 
-	public Order(String code) {
+	/**
+	 * Construtor da classe pedido
+	 * 
+	 * @param code 			código do pedido
+	 * @param totalValue	valor total do pedido
+	 */
+	public Order(String code, BigDecimal totalValue) {
 		this.code = code;
+		this.totalValue = totalValue;
 	}
 
-	@Override
-	public String toString() {
-		return "Order={" + "code='" + code + "'" + "}";
+	/**
+	 * Calcula as taxa de acordo com o valor do pedido. Se o valor for maior que 100, uma taxa será cobrada
+	 * 
+	 * @return						valor total do pedido com as taxas
+	 * @throws RuntimeException		se o pedido for negativo
+	 */
+	public BigDecimal calculateFee() throws RuntimeException {
+		if(this.totalValue.signum() < 0) {
+			System.out.println("teste");
+			throw new RuntimeException("O pedido não pode ter valor negativo.");
+		}
+		if (this.totalValue.compareTo(new BigDecimal("100.00")) > 100) {
+			System.out.println("teste");
+			return this.totalValue.multiply(new BigDecimal("0.99"));
+		}
+		System.out.println("teste");
+		return this.totalValue;
 	}
+	/*
+	@Override
+    public String toString() {
+            return "Order={" + "code='" + code + "' | Valor=R$ " + totalValue + "}";
+    }*/
 }
